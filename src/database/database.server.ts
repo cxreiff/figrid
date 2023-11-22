5aaee1a69dc3a7eaa357c1e73ddc5e63a07589be
cxreiff <cooper@cxreiff.com>
2023-11-22 02:52:16 -0800

    attempt to fix edge function issue

--- src/database/database.server.ts
+++ src/database/database.server.ts
@@ -1,11 +1,11 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { remember } from "@epic-web/remember";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("Missing environment variable: DATABASE_PATH");
+  throw new Error("Missing environment variable: DATABASE_URL");
 }
 
 export const db = remember("db", () =>
-  drizzle(postgres(process.env.DATABASE_URL)),
+  drizzle(new Pool({ connectionString: process.env.DATABASE_URL })),
 );
