5315bf2c0e8ffd6bc666d0de2177c24de94396b4
cxreiff <cooper@cxreiff.com>
2023-11-26 03:28:15 -0800

    turns out i actually need that redirect

--- src/routes/auth.$strategy.ts
+++ src/routes/auth.$strategy.ts
@@ -1,11 +1,18 @@
-import { type ActionFunctionArgs } from "@vercel/remix"
+import { redirect, type ActionFunctionArgs } from "@vercel/remix"
 import { z } from "zod"
 import {
     ROUTE_STRATEGY_MAP,
     authenticator,
 } from "~/auth/authenticator.server.ts"
 
+export async function loader() {
+    return redirect("/auth/login")
+}
+
 export async function action({ request, params }: ActionFunctionArgs) {
     const { strategy } = z.object({ strategy: z.string() }).parse(params)
-    return authenticator.authenticate(ROUTE_STRATEGY_MAP[strategy], request)
+    return authenticator.authenticate(ROUTE_STRATEGY_MAP[strategy], request, {
+        successRedirect: "/protected",
+        failureRedirect: "/auth/login",
+    })
 }
