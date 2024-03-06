80e9c113a991d0870ef3e46b1a9e42a9865db0c1
cxreiff <cooper@cxreiff.com>
2024-03-06 02:53:00 -0800

    swapped DB 'connect' to 'Client'

--- src/database/database.server.ts
+++ src/database/database.server.ts
@@ -1,6 +1,6 @@
 import { remember } from "@epic-web/remember"
 import { drizzle } from "drizzle-orm/planetscale-serverless"
-import { connect } from "@planetscale/database"
+import { Client } from "@planetscale/database"
 import * as assetsSchema from "~/database/schema/assets.server.ts"
 import * as authSchema from "~/database/schema/auth.server.ts"
 import * as charactersSchema from "~/database/schema/characters.server.ts"
@@ -13,7 +13,7 @@ import * as locksSchema from "~/database/schema/locks.server.ts"
 import * as tilesSchema from "~/database/schema/tiles.server.ts"
 
 export const db = remember("db", () =>
-    drizzle(connect({ url: process.env.DATABASE_URL }), {
+    drizzle(new Client({ url: process.env.DATABASE_URL }), {
         schema: {
             ...assetsSchema,
             ...authSchema,
